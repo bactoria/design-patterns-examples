fc1da5771cdba510fe5a43fa0708915de9f043ba
bactoria <bactoria@gmail.com>
2021-12-26 00:27:18 +0900

    ADD:: 팩토리 메서드 코드 추가

---
+++ src/main/kotlin/me/bactoria/creational/factorymethod/_Main.kt
@@ -0,0 +1,32 @@
+package me.bactoria.creational.factorymethod
+
+import java.util.*
+
+fun main() {
+	val glenfiddich = WhiskeyFactory.createWhiskey("Glenfiddich")
+	val label5 = WhiskeyFactory.createWhiskey("Label5")
+	val bombay = WhiskeyFactory.createWhiskey("Bombay")
+
+	println(glenfiddich)
+	println(label5)
+	println(bombay)
+}
+
+object WhiskeyFactory {
+	fun createWhiskey(name: String): Whiskey {
+		return when (name.trim().lowercase(Locale.getDefault())) {
+			"glenfiddich" -> Glenfiddich()
+			"label5" -> Label5()
+			"bombay" -> Bombay()
+			else -> throw IllegalArgumentException("지원하지 않는 위스키입니다. name: $name")
+		}
+	}
+}
+
+interface Whiskey
+
+class Glenfiddich : Whiskey
+
+class Label5 : Whiskey
+
+class Bombay : Whiskey
